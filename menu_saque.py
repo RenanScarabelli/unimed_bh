739f1c866fc13a8b4e20adb71bc9d72abf2ad5cc
RenanScarabelli <renanscarabelli@gmail.com>
2022-10-10 00:45:17 -0300

    Modificação para melhor visualização.

---
+++ menu_saque.py
@@ -0,0 +1,26 @@
+saldo = 2500
+limite = 1000
+opcao = -1
+
+saque = float(input("Informe o valor para saque: "))
+
+if saque <= saldo:
+    saldo_final = saldo - saque
+    print("Saque realizado com sucesso.")
+    print(f"Saldo atual é: {saldo_final:.2f}")
+    saldo = saldo_final
+
+elif saque <= saldo + limite:
+    opcao = int(input("Saldo insuficiente, deseja utilizar seu limite? \n [1] Sim \n [2] Não \n: "))
+    
+    if opcao == 1:
+        saldo_final = saldo - saque
+        print("Saque realizado com sucesso.")
+        print(f"Saldo atual é: {saldo_final:.2f}")
+        saldo = saldo_final
+    else:
+        print("Saque não realizado")
+elif saque >= saldo + limite:
+    print("Saldo + Limite insuficiente.")
+
+print("Fim da operação.")
